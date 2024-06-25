641cbb410f4e573454664cc1807575d3185c46e5
Kayler <kaylermoura.km@gmail.com>
2024-06-25 15:09:17 -0300

    Translating all comments to english

--- main.py
+++ main.py
@@ -6,60 +6,64 @@ from PIL import Image, ImageTk
 from urllib.request import urlopen
 import io
 
+# Threads a new download
 def start_download_thread():
     thread = threading.Thread(target=start_download)
     thread.start()
 
+# Handles all the download process and calls for on-screen updates
 def start_download():
     try:
         youtube_link = link.get()
         youtube_object = YouTube(youtube_link, on_progress_callback=on_progress)
         
-        # Atualiza o título do vídeo e a thumbnail
+        # Updates title and thumbnail based on video link info
         update_video_thumbnail(youtube_object.title, youtube_object.thumbnail_url)
         
-        choice = combobox.get()  # Obtém a escolha do dropdown
+        choice = combobox.get()  # Gets the dropdown menu choice
 
         if choice == 'Audio':
             audio = youtube_object.streams.get_audio_only()
-            audio.download(output_path="./Audio Downloads")
+            audio.download(output_path="./Audio Downloads") # type: ignore
             download_label.configure(text="Audio download completed!", text_color="white")
         
         elif choice == 'Video':
             video = youtube_object.streams.get_highest_resolution()
-            video.download(output_path="Video Downloads")
+            video.download(output_path="Video Downloads") # type: ignore
             download_label.configure(text="Video download completed!", text_color="white")
 
         title.configure(text=youtube_object.title)  # Mostra o título do vídeo sendo baixado
         title.update()
-        progress_percentage.configure(text='100%')  # Atualiza a porcentagem para 100% no final do download
-        progress_bar.set(1.0)  # Define a barra de progresso como completa
+        progress_percentage.configure(text='100%')  # Updates final percentage to 100%
+        progress_bar.set(1.0) # Sets progress bar as complete
     except Exception as e:
         download_label.configure(text='Invalid Youtube link', text_color="red")
 
+# Sets new thumbnail on display
 def update_video_thumbnail(video_title, thumbnail_url):
     # Atualiza o título do vídeo
     title.configure(text=video_title)
 
     # Atualiza a thumbnail do vídeo
-    image_bytes = urlopen(thumbnail_url).read()  # Lê os bytes da imagem da URL
-    image_data = io.BytesIO(image_bytes)  # Converte para um buffer de bytes
+    image_bytes = urlopen(thumbnail_url).read()  # Read bytes from url
+    image_data = io.BytesIO(image_bytes)  # Buffer bytes
 
     pil_image = Image.open(image_data)  # Abre a imagem usando PIL
-    tk_image = customtkinter.CTkImage(pil_image, size=(192, 108))  # Converte a imagem para um formato suportado por tkinter
+    tk_image = customtkinter.CTkImage(pil_image, size=(192, 108))  # Converts image to supported format
 
-    image_label.configure(image=tk_image, pady=15)  # Atualiza a imagem no label (thumbnail)
+    image_label.configure(image=tk_image, pady=15)  # Updates image label (thumbnail)
     image_label.update()
 
+# Updates on-screen download progress info
 def on_progress(stream, chunk, bytes_remaining):
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     completion_percentage = bytes_downloaded / total_size * 100
 
-    progress_percentage.configure(text=f'{int(completion_percentage)} %')  # Atualiza a porcentagem de download na tela
+    progress_percentage.configure(text=f'{int(completion_percentage)} %')  # Updates percentage bar display
     progress_percentage.update()
 
-    progress_bar.set(completion_percentage / 100)  # Atualiza a barra de progresso
+    progress_bar.set(completion_percentage / 100)  # Updates progress bar percentage
 
 # System settings
 customtkinter.set_appearance_mode("System")
